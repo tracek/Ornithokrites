2bf4a1bb85abd2a68843211988964e0cc20a6678
tracek <lukasz.tracewski@gmail.com>
2014-02-26 22:17:59 +0100

    Some refactoring and better model

--- ornithokrites.py
+++ ornithokrites.py
@@ -15,25 +15,31 @@ import noise_reduction
 import features
 import identification
 
-app_config = configuration.Configurator().parse_arguments()
-reporter = reporting.Reporter(location=app_config.data_store, write_to_stdout=app_config.write_stdout)
-walker = recordings_io.get_recordings_walker(data_store=app_config.data_store, bucket=app_config.bucket)
-kiwi_finder = identification.KiwiFinder()
-
-for rate, sample, sample_name in walker.read_wave(): 
-    noise_remover = noise_reduction.NoiseRemover()
-    try:
-        filtered_sample = noise_remover.remove_noise(sample, rate)
-    except ValueError:
-        filtered_sample = sample
-
-    segmented_sounds = noise_remover.segmentator.get_segmented_sounds()
+class Ornithokrites(object):
     
-    feature_extractor = features.FeatureExtractor()
-    extracted_features = feature_extractor.process(filtered_sample, rate, segmented_sounds)
-    
-    kiwi_calls = kiwi_finder.find_individual_calls(extracted_features)
-    result_per_file = kiwi_finder.find_kiwi(kiwi_calls)
-    reporter.write_results(result_per_file, kiwi_calls, sample_name, filtered_sample, rate, segmented_sounds)
+    def run(self):
+        app_config = configuration.Configurator().parse_arguments()
+        reporter = reporting.Reporter(location=app_config.data_store, write_to_stdout=app_config.write_stdout)
+        walker = recordings_io.get_recordings_walker(data_store=app_config.data_store, bucket=app_config.bucket)
+        kiwi_finder = identification.KiwiFinder()
+        
+        for rate, sample, sample_name in walker.read_wave(): 
+            noise_remover = noise_reduction.NoiseRemover()
+            try:
+                filtered_sample = noise_remover.remove_noise(sample, rate)
+            except ValueError:
+                filtered_sample = sample
+        
+            segmented_sounds = noise_remover.segmentator.get_segmented_sounds()
+            
+            feature_extractor = features.FeatureExtractor()
+            extracted_features = feature_extractor.process(filtered_sample, rate, segmented_sounds)
+            
+            kiwi_calls = kiwi_finder.find_individual_calls(extracted_features)
+            result_per_file = kiwi_finder.find_kiwi(kiwi_calls)
+            reporter.write_results(result_per_file, kiwi_calls, sample_name, filtered_sample, rate, segmented_sounds)
 
-reporter.cleanup()
+        reporter.cleanup()
+
+if __name__ == '__main__':
+    Ornithokrites().run()
