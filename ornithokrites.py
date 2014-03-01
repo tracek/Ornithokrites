062f84eae4aa22895e2bac6b41ab06cbe8a88fc8
tracek <lukasz.tracewski@gmail.com>
2014-03-01 11:49:54 +0100

    Fix #3

--- ornithokrites.py
+++ ornithokrites.py
@@ -10,19 +10,13 @@ Identification of kiwi calls from audio recordings - main module.
 
 import configuration
 import reporting
-import recordings_io
 import noise_reduction
 import features
 import identification
 import s3connection
 
 
-class MassiveOrnithokrites(object):
-    """ This class is meant as a replacement of original Ornithokrites. Purpose is to make processing
-        of data on-demand: file is not downloaded unless at given moment it needs to be processed. 
-        Another advantage is that we can enable parallel processing: there are no reasons why we should
-        not work in parallel on number of samples equal to number of CPU cores """
-
+class Ornithokrites(object):
     def run(self):    
         app_config = configuration.Configurator().parse_arguments()
         reporter = reporting.Reporter(location=app_config.data_store, write_to_stdout=app_config.write_stdout)
@@ -46,33 +40,7 @@ class MassiveOrnithokrites(object):
             result_per_file = kiwi_finder.find_kiwi(kiwi_calls)
             reporter.write_results(result_per_file, kiwi_calls, sample_name, filtered_sample, 
                                    rate, segmented_sounds, app_config.keep_data)
-
-
-class Ornithokrites(object):
-    
-    def run(self):
-        app_config = configuration.Configurator().parse_arguments()
-        reporter = reporting.Reporter(location=app_config.data_store, write_to_stdout=app_config.write_stdout)
-        walker = recordings_io.get_recordings_walker(data_store=app_config.data_store, bucket=app_config.bucket)
-        kiwi_finder = identification.KiwiFinder()
-        
-        for rate, sample, sample_name in walker.read_wave(): 
-            noise_remover = noise_reduction.NoiseRemover()
-            try:
-                filtered_sample = noise_remover.remove_noise(sample, rate)
-            except:
-                filtered_sample = sample
-        
-            segmented_sounds = noise_remover.segmentator.get_segmented_sounds()
-            
-            feature_extractor = features.FeatureExtractor()
-            extracted_features = feature_extractor.process(filtered_sample, rate, segmented_sounds)
-            
-            kiwi_calls = kiwi_finder.find_individual_calls(extracted_features)
-            result_per_file = kiwi_finder.find_kiwi(kiwi_calls)
-            reporter.write_results(result_per_file, kiwi_calls, sample_name, filtered_sample, rate, segmented_sounds)
-
         reporter.cleanup()
 
 if __name__ == '__main__':
-    MassiveOrnithokrites().run()
+    Ornithokrites().run()
